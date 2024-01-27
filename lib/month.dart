9ffd8655730cb42ca611e0704a16109b6bb6bfa3
Jamin Eisenberg <109154023+jeisenberg-chwy@users.noreply.github.com>
2024-01-27 13:57:51 -0500

    Display expenses in month

--- lib/month.dart
+++ lib/month.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:collection/collection.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -9,11 +10,15 @@ class Month extends StatelessWidget {
   final List<Expense> expenses;
   final num? allAccountsTotal;
 
-  const Month({super.key, required this.month, required this.expenses, this.allAccountsTotal});
+  const Month(
+      {super.key,
+      required this.month,
+      required this.expenses,
+      this.allAccountsTotal});
 
   static final CollectionReference<Map<String, dynamic>> dbCollection =
-  FirebaseFirestore.instance
-      .collection('users/${FirebaseAuth.instance.currentUser!.uid}/months');
+      FirebaseFirestore.instance
+          .collection('users/${FirebaseAuth.instance.currentUser!.uid}/months');
 
   static String format(DateTime month) {
     return "${month.month.toString().padLeft(2, "0")}/${month.year}";
@@ -25,12 +30,14 @@ class Month extends StatelessWidget {
       title: Row(
         children: [
           Text(format(month)),
-          // SizedBox.fromSize(size: const Size(10, 0)),
-          // Text(bucketName),
-          // const Spacer(),
-          // Text(Expense.formattedCost(amountCents)),
+          const Spacer(),
+          Text(
+              "Total expenses: ${Expense.formattedCost(expenses.map((e) => e.centsCost).sum)}"),
         ],
       ),
+      subtitle: allAccountsTotal == null
+          ? null
+          : Text(Expense.formattedCost(allAccountsTotal!)),
     );
   }
 }
